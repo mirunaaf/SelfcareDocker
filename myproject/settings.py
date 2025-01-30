6e1f71836cd8b74b494ad1be5d24f9ae344b5266
Zukhra <zuhra.galieva.2014@gmail.com>
2025-01-30 18:02:59 +0000

    added the image to the index.html

--- myproject/settings.py
+++ myproject/settings.py
@@ -130,8 +130,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'myapp/static'),  # Ensure correct path
+    os.path.join(BASE_DIR, 'myapp/static'),
 ]
+
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field

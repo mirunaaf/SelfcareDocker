eadbdf893cddb440fe5e5d7edf21b1afb5024bac
Zukhra <zuhra.galieva.2014@gmail.com>
2025-01-31 10:50:39 +0000

    Added the menu.html page

--- myapp/views.py
+++ myapp/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,"myapp/index.html",{})
+
+def menu(request):
+    return render(request,"myapp/menu.html",{})
+

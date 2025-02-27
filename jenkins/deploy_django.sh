67ef417a134b7156949233aac05602e338bcb243
mirunaaf <filfan.miruna@gmsil.com>
2025-02-27 21:32:27 +0000

    copy docker-compose to deploy-ec2

--- jenkins/deploy_django.sh
+++ jenkins/deploy_django.sh
@@ -4,6 +4,9 @@ EC2_USER="ec2-user"
 EC2_IP="18.169.247.44"
 SSH_KEY="/var/lib/jenkins/.ssh/id_rsa"
 
+echo "Copying docker-compose.yml to deployment server..."
+scp -i $SSH_KEY docker-compose.yml $EC2_USER@$EC2_IP:~/docker-compose.yml
+
 echo "Stopping existing containers on deployment server..."
 ssh -o StrictHostKeyChecking=no -i $SSH_KEY $EC2_USER@$EC2_IP "docker-compose down"
 

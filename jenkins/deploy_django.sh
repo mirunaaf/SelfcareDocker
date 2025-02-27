8357b7a4ce6e7fdf25eb332a3cbbd84df2d04ba9
mirunaaf <filfan.miruna@gmsil.com>
2025-02-27 16:23:06 +0000

    credential change

--- jenkins/deploy_django.sh
+++ jenkins/deploy_django.sh
@@ -2,12 +2,13 @@
 
 EC2_USER="ec2-user"
 EC2_IP="18.169.247.44"
+SSH_KEY="/home/ec2-user/.ssh/id_rsa"
 
-echo "Stopping existing containers..."
-ssh -o StrictHostKeyChecking=no -i key.pem $EC2_USER@$EC2_IP "docker-compose down"
+echo "Stopping existing containers on deployment server..."
+ssh -o StrictHostKeyChecking=no -i $SSH_KEY $EC2_USER@$EC2_IP "docker-compose down"
 
 echo "Pulling latest Docker images..."
-ssh -o StrictHostKeyChecking=no -i key.pem $EC2_USER@$EC2_IP "docker pull ${{ secrets.DOCKER_USERNAME }}/django-app:latest"
+ssh -o StrictHostKeyChecking=no -i $SSH_KEY $EC2_USER@$EC2_IP "docker pull $DOCKER_USERNAME/django-app:latest"
 
 echo "Starting Django container..."
-ssh -o StrictHostKeyChecking=no -i key.pem $EC2_USER@$EC2_IP "docker-compose up -d"
+ssh -o StrictHostKeyChecking=no -i $SSH_KEY $EC2_USER@$EC2_IP "docker-compose up -d"

6b1fab0d5fb20f60bfa533b0bbd323d0c7443743
mirunaaf <filfan.miruna@gmsil.com>
2025-02-27 22:05:35 +0000

    export DOCKER_USERNAME

--- jenkins/deploy_django.sh
+++ jenkins/deploy_django.sh
@@ -5,13 +5,13 @@ EC2_IP="18.169.247.44"
 SSH_KEY="/var/lib/jenkins/.ssh/id_rsa"
 
 echo "Copying docker-compose.yml to deployment server..."
-scp -i $SSH_KEY docker-compose.yml $EC2_USER@$EC2_IP:~/docker-compose.yml
+scp -i $SSH_KEY /var/lib/jenkins/workspace/deploy-django/docker-compose.yml $EC2_USER@$EC2_IP:~/
 
 echo "Stopping existing containers on deployment server..."
-ssh -o StrictHostKeyChecking=no -i $SSH_KEY $EC2_USER@$EC2_IP "docker-compose down"
+ssh -o StrictHostKeyChecking=no -i $SSH_KEY $EC2_USER@$EC2_IP "export DOCKER_USERNAME=$DOCKER_USERNAME && docker-compose down"
 
 echo "Pulling latest Docker images..."
-ssh -o StrictHostKeyChecking=no -i $SSH_KEY $EC2_USER@$EC2_IP "docker pull $DOCKER_USERNAME/django-app:latest"
+ssh -o StrictHostKeyChecking=no -i $SSH_KEY $EC2_USER@$EC2_IP "export DOCKER_USERNAME=$DOCKER_USERNAME && docker pull $DOCKER_USERNAME/django-app:latest"
 
 echo "Starting Django container..."
-ssh -o StrictHostKeyChecking=no -i $SSH_KEY $EC2_USER@$EC2_IP "docker-compose up -d"
+ssh -o StrictHostKeyChecking=no -i $SSH_KEY $EC2_USER@$EC2_IP "export DOCKER_USERNAME=$DOCKER_USERNAME && docker-compose up -d"
